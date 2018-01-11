79e05dca89e5095bdc67f0b889160eefe295cc08
Ingo Ruhnke <grumbel@gmail.com>
2018-01-11 20:47:40 +0100

    Added author/url fields to setup.py

--- setup.py
+++ setup.py
@@ -23,6 +23,11 @@ from setuptools import setup, find_packages
 setup(
     name='procmem',
     version='0.1.0',
+
+    author="Ingo Ruhnke",
+    author_email="grumbel@gmail.com",
+    url="https://github.com/Grumbel/procmem",
+
     scripts=[],
     entry_points={
         'console_scripts': [
